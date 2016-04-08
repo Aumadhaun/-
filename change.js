1823232614c65fc9a371fbf6193b4a6bd5d1f29a
Aumadhaun <angelskaya_pyl@mail.ru>
2016-04-08 21:06:23 +0300

    все, идет рандомный цвет

--- change.js
+++ change.js
@@ -53,8 +53,7 @@ var g = Math.floor(Math.random()*256);
 var b = Math.floor(Math.random()*256);
 var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
 context.strokeStyle = rgb;
-} ;
-
+};
 	
 	/*gradientColor = function(color){
 		context.strokeStyle = ('rgba(255, 0, 0, 0.000)');
